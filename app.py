52551f928d25d9fea1a5a3e06adb5bdecab35966
wandreuscv <wandreuscv@unipam.edu.br>
2020-01-22 21:51:14 -0300

    Pagina de produtos

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
+from data import Products
 
 app = Flask(__name__)
+
+Products = Products()
 
 @app.route('/')
 def index():
     return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/products')
+def products():
+    return render_template('products.html', products = Products)
+
+@app.route('/product/<string:id>/')
+def product(id):
+    return render_template('product.html', id = id)
 
 if __name__ == '__main__':
     app.run(debug=True)
